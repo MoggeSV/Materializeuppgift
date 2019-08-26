a0c07ec5cee812c72fc0d74e95ca4cd79bf2d358
Morgan Svensson <morgan.svensson10@gmail.com>
2019-08-26 12:32:30 +0200

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeCarouselComponent } from './welcome-carousel/welcome-carousel.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: WelcomeCarouselComponent }
+  { path: '', component: WelcomeCarouselComponent },
+  { path: 'orders', component: OrdersComponent},
+  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
