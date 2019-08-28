2a3461ffbc86574d44519abe32ca19b4494cf1db
abdulrazaq <abdulrazaq.me@gmail.com>
2019-08-28 14:10:40 +0200

    profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import users from '../data/users.json';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+users: any = users;
+currentUser: any;
 
+text: String = "test"
   constructor() { }
 
   ngOnInit() {
+    this.initUser();
   }
 
+  test = function() {
+    console.log(this.currentUser);
+  }
+
+  initUser = function() {
+    this.currentUser = users[0];
+  }
 }
