f57db7a0de97c1c25946a9366a80e266ca96b591
Filip Glisovic <filipglisovic@laptop-0io69vqc.learnet.se>
2019-08-28 12:45:55 +0200

    Added profucts page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { OrdersComponent } from './orders/orders.component';
 import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { CartComponent } from './cart/cart.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeCarouselComponent },
   { path: 'orders', component: OrdersComponent},
   { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginComponent},
-  { path: 'cart', component: CartComponent}
+  { path: 'cart', component: CartComponent},
+  { path: 'products', component: ProductsComponent}
+
 ];
 
 @NgModule({
