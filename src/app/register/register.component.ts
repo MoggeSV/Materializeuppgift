7d543ef40a15c57f5bd3ce67d646efe25d4cfcf8
Christoffer <astrom56@hotmail.com>
2019-09-02 13:15:12 +0200

    Added SASS and fixed errors in Register Component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  form: FormGroup
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      firstname: new FormControl(''),
+      lastname: new FormControl(''),
+      address: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
   }
 
 }
